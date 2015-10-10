1941416836f2f1f9b51d7093a273c49d2f7c4e2b
Serg09 <sergeyskumatov@aol.com>
2015-10-10 10:09:41 -0600

    Edit bottles_of_beer.rb file
    [#105171276]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -4,9 +4,10 @@ def bottles(beer)
   return 0 if beer == -1
   br = beer - 1
   beer != 1 ? wd = 'bottles' : wd = 'bottle'
-  if beer == 0
-    beer = 'no more'
-  end
+  beer == 0 ? beer : 'no more'
+  # if beer == 0
+  #   beer = 'no more'
+  # end
   print "#{beer} #{wd} of beer on the wall,".capitalize
   print " #{beer} #{wd} of beer. \n".capitalize
   print 'Take one down and pass it around,'
