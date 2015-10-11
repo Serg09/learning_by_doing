7abef8345e77beed23f91c2289a80b114a1c3b4b
Serg09 <sergeyskumatov@aol.com>
2015-10-10 21:22:53 -0600

    Edit Readme.md file
    short description
    
    [#105171276]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -8,6 +8,12 @@ def bottles(beer)
 
   beer == 1 ? count_down = 'no more' : count_down = beer - 1
 
+  print_verse(beer, word, word_2, count_down)
+
+  bottles(beer - 1)
+end
+
+def print_verse(beer, word, word_2, count_down)
   if beer != 0
     s = "#{beer} #{word}"
     c = 'Take one down and pass it around,'
@@ -16,12 +22,6 @@ def bottles(beer)
     c = 'Go to the store and buy some more,'
   end
 
-  print_verse(s, c, word_2, count_down)
-
-  bottles(beer - 1)
-end
-
-def print_verse(s, c, word_2, count_down)
   print "#{s.capitalize} of beer on the wall, #{s} of beer. \n"
   print " #{c} #{count_down} #{word_2}".gsub('-1', '99')
   print " of beer on the wall. \n\n"
