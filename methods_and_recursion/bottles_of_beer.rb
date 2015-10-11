f5ed082bbfbf177048dd2536e74ee74e6e466515
Serg09 <sergeyskumatov@aol.com>
2015-10-10 20:39:03 -0600

    Edit Readme.md and bottles_of Beer.rb files
    [#105171276]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -2,18 +2,29 @@
 
 def bottles(beer)
   return 0 if beer == -1
-  br = beer - 1
-  beer != 1 ? wd = 'bottles' : wd = 'bottle'
-  beer == 0 ? beer : 'no more'
-  # if beer == 0
-  #   beer = 'no more'
-  # end
-  print "#{beer} #{wd} of beer on the wall,".capitalize
-  print " #{beer} #{wd} of beer. \n".capitalize
-  print 'Take one down and pass it around,'
-  print " #{br} #{wd} of beer on the wall. \n\n".gsub('-1', '99').capitalize
 
-  beer * bottles(beer - 1)
+  beer != 1 ? word = 'bottles' : word = 'bottle'
+  (beer - 1) != 1 ? word_2 = 'bottles' : word_2 = 'bottle'
+
+  count_down = beer - 1
+
+  count_down = 'no more' if count_down == 0
+
+  if beer != 0
+    s = "#{beer} #{word}"
+    c = 'Take one down and pass it around,'
+  else
+    s = "no more #{word}"
+    c = 'Go to the store and buy some more,'
+  end
+
+  print "#{s}".capitalize + ' of beer on the wall,'
+  print " #{s} of beer. \n"
+  print c
+  print " #{count_down} #{word_2}".gsub('-1', '99')
+  print " of beer on the wall. \n\n"
+
+  bottles(beer - 1)
 end
 
 bottles(99)
