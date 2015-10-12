5a4c09364019a9df1d53e7902b3ba45885d429b2
Serg09 <sergeyskumatov@aol.com>
2015-10-12 02:12:46 -0600

    Edit FizzBuzz array version.
    [#105438188]

---
+++ arrays/Readme.md
@@ -0,0 +1,3 @@
+Edit fizz_buzz_arrays
+
+Rubocop complain about my array method. Did change it to the rubocop suggestions. Did not really understood the reason of such changes.
--- arrays/fizz_buzz_arrays.rb
+++ arrays/fizz_buzz_arrays.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
-  fizzbuzz = ['Fizz' 'Buzz' 'FizzBuzz']
 numbers = [*1..100]
 numbers.each do |number|
+  fizz_buzz = %w(Fizz Buzz FizzBuzz)
   if (number % 15) == 0
-    puts fizzbuzz[0]
+    puts fizz_buzz[0]
   elsif (number % 3) == 0
-    puts fizzbuzz[1]
+    puts fizz_buzz[1]
   elsif (number % 5) == 0
-    puts fizzbuzz[2]
+    puts fizz_buzz[2]
   else
     puts number.to_s
   end
