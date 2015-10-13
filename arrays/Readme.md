27cc6216fd0b2aa968bbecf7df44f97d42c5c9cc
Serg09 <sergeyskumatov@aol.com>
2015-10-12 23:03:38 -0600

    Finished Arrays home work one
    [#105353658]

--- arrays/Readme.md
+++ arrays/Readme.md
@@ -1,3 +1,2 @@
 Do not have any offenses. But got problem with Travis. 
-The expected line "- T-10, 9, 8, 7, 6, 5, 4, 3, 2, 1... BLASTOFF!"
-is same as in my output, but still getting error. Could not figure out the problem.
+Took some time and help to figure out how to fix it
--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -2,22 +2,22 @@
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-puts '- ' + numbers.join('...').to_s + '...'
+puts numbers.join('...').to_s + '...'
 # puts '- T-10, 9, 8, 7, 6, 5, 4, 3, 2, 1... BLASTOFF!'
-puts "- T-#{numbers.reverse.join(',')} ... BLASTOFF!"
-puts '- The last element is ' + numbers.last.to_s
-puts '- The first element is ' + numbers.first.to_s
-puts '- The third element is ' + numbers[2].to_s
-puts '- The element with an index ' + 3.to_s + ' is ' + numbers[3].to_s
-puts '- The second from last element is ' + numbers[8].to_s
-puts "- The first four elements are '#{numbers[0..3].join(', ')}'"
-print "- If we delete #{numbers.delete(5)}, #{numbers.delete(6)} and"
+puts "T-#{numbers.reverse.join(', ')}...  BLASTOFF!"
+puts 'The last element is ' + numbers.last.to_s
+puts 'The first element is ' + numbers.first.to_s
+puts 'The third element is ' + numbers[2].to_s
+puts 'The element with an index of ' + 3.to_s + ' is ' + numbers[3].to_s
+puts 'The second from last element is ' + numbers[8].to_s
+puts "The first four elements are '#{numbers[0..3].join(', ')}'"
+print "If we delete #{numbers.delete(5)}, #{numbers.delete(6)} and"
 print " #{numbers.delete(7)} from the array, "
 puts "we're left with [#{numbers.join(',')}]"
-print '- If we add 5 at the beginning of the array, '
+print 'If we add 5 at the beginning of the array, '
 puts "we're left with [#{numbers.unshift(5).join(',')}]"
-print '- If we add 6 to the end of the array, '
+print 'If we add 6 at the end of the array, '
 puts "we're left with [#{numbers.push(6).join(',')}]"
 puts "- Only the elements #{numbers.select { |n| n > 8 }} are > #{numbers[5]}."
-print '- If we remove all the elements, '
+print 'If we remove all the elements, '
 puts "then the length of the array is #{numbers.clear.length}"
