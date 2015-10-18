bffafee3ca337ca1a493c19c18516240df23a8a8
Serg09 <sergeyskumatov@aol.com>
2015-10-18 16:41:02 -0600

    More steps done
    [#105929840]

--- objects_tdd/spec/lib/cashregister_spec.rb
+++ objects_tdd/spec/lib/cashregister_spec.rb
@@ -1,15 +1,23 @@
-require './lib/cashregister'
+require './lib/cash_register'
 
 describe CashRegister do
-  describe '.initialize(0.00)' do
+  describe '#total(0.00)' do
     it 'returns 0.00 for a total price' do
       expect(CashRegister.initialize).to be_truthy
     end
-
-    # def '.purchase(price)' do
-    describe '.purchase(price)' do
+    #
+    # # def '.purchase(price)' do
+    describe '#purchase(price)' do
+      context 'purchase'
       it 'returns sum of total and purchase price' do
-        expect(CashRegister.purchase(price)).to be_truthy
+        expect(CashRegister.purchase(price)).to be_truthy #??
+      end
+
+      describe '#pay(payment)' do
+        context 'pay'
+        it 'returns sum subtracted from total' do
+          # expect(CashRegister.purchase(price)).to be_truthy #??
+        end
       end
     end
   end
