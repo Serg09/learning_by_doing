e0f837db37e0ad8043d4b224fb5486af47f543d3
Serg09 <sergeyskumatov@aol.com>
2015-10-18 00:04:35 -0600

    Correct errors in bottles_of_beer.rb
    [#105171276]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,28 +1,26 @@
 #!/usr/bin/env ruby
 
 def bottles(beer)
-  return 0 if beer == -1
-
-  beer != 1 ? word = 'bottles' : word = 'bottle'
+  word = beer == 1 ? 'bottle' : 'bottles' # !
   (beer - 1) != 1 ? word_2 = 'bottles' : word_2 = 'bottle'
 
   beer == 1 ? count_down = 'no more' : count_down = beer - 1
 
   print_verse(beer, word, word_2, count_down)
 
-  bottles(beer - 1)
+  bottles(beer - 1) if beer >= 1 # !
 end
 
-def print_verse(beer, word, word_2, count_down)
-  if beer != 0
-    comb = "#{beer} #{word}"
+def print_verse(bottles_of_beer, word, word_2, count_down)
+  if bottles_of_beer != 0
+    wording = "#{bottles_of_beer} #{word}"
     txt = 'Take one down and pass it around,'
   else
-    comb = "no more #{word}"
+    wording = "no more #{word}"
     txt = 'Go to the store and buy some more,'
   end
 
-  print "#{comb.capitalize} of beer on the wall, #{comb} of beer. \n"
+  print "#{wording.capitalize} of beer on the wall, #{wording} of beer. \n"
   print " #{txt} #{count_down} #{word_2}".gsub('-1', '99')
   print " of beer on the wall. \n\n"
 end
