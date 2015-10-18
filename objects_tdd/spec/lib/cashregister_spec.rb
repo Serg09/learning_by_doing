63520114e366fd5f48031a04e7d7700490a177ed
Serg09 <sergeyskumatov@aol.com>
2015-10-18 15:31:58 -0600

    More changes
    [#105929840]

--- objects_tdd/spec/lib/cashregister_spec.rb
+++ objects_tdd/spec/lib/cashregister_spec.rb
@@ -3,7 +3,7 @@ require './lib/cashregister'
 describe CashRegister do
   describe 'initialize' do
     it 'returns 0.00 for a total price' do
-      expect(CashRegister.initialize(0.00)).to be_truthy
+      expect(CashRegister.initialize('0.00')).to be_truthy
     end
   end
 end
