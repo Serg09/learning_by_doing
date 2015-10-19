1605550ca2813068aad076bf31136d7e70d96b1e
Serg09 <sergeyskumatov@aol.com>
2015-10-18 23:21:54 -0600

    Did more changes to the code
    [#105929840]

--- objects_tdd/lib/cash_register.rb
+++ objects_tdd/lib/cash_register.rb
@@ -3,24 +3,24 @@ class CashRegister
   def self.initialize
     @total = 0.00
   end
-end
 
-def purchase(price)
-  @total += price
-end
+  def purchase(price)
+    @total += price
+  end
 
-def pay(payment)
-  @total += payment
-end
+  def pay(payment)
+    @total -= payment
+  end
 
-def total
-  format('%.2f', @total) # two decimals format
+  def total
+    format('%.2f', @total) # two decimals format
+  end
 end
 
 # register = CashRegister.new
-#
-# puts register.total # set to 0
+
+# register.total # set to 0
 # register.purchase(3.78)
-# puts register.total # return cash + purchase
+# register.total # return cash + purchase
 # register.pay(5.00)
-# puts register.total
+# register.total
--- objects_tdd/spec/lib/cashregister_spec.rb
+++ objects_tdd/spec/lib/cashregister_spec.rb
@@ -1,20 +1,26 @@
 require './lib/cash_register'
 
 describe CashRegister do
-  describe '#total(0.00)' do
+  describe 'initialize' do
     it 'returns 0.00 for a total price' do
-      expect(CashRegister.initialize).to be_truthy
+      expect(CashRegister.initialize).to eq(0.00)
     end
 
     describe '#purchase(price)' do
       context 'purchase'
       it 'returns sum of total and purchase price' do
+        def purchase(price)
+          @total += price
+        end
         # expect(CashRegister.purchase(price)).to be_truthy # ??
       end
 
       describe '#pay(payment)' do
         context 'pay'
         it 'returns sum subtracted from total' do
+          def pay(payment)
+            @total -= payment
+          end
           # expect(CashRegister.pay(payment)).to be_truthy # ??
         end
       end
