78a6969cba6a0ddc97c46202b625e29b67da1252
Serg09 <sergeyskumatov@aol.com>
2015-10-20 21:56:49 -0600

    TDD CasgRegistry
    [#105929840]

--- objects_tdd/lib/cash_register.rb
+++ objects_tdd/lib/cash_register.rb
@@ -1,7 +1,8 @@
 # Cash Register
 class CashRegister
-  def self.initialize
-    @total = 0.00
+  attr_reader :total
+  def initialize
+    @total = (0.00)
   end
 
   def purchase(price)
@@ -13,14 +14,6 @@ class CashRegister
   end
 
   def total
-    format('%.2f', @total) # two decimals format
+    @total.round(2) # two decimals format
   end
 end
-
-# register = CashRegister.new
-
-# register.total # set to 0
-# register.purchase(3.78)
-# register.total # return cash + purchase
-# register.pay(5.00)
-# register.total
--- objects_tdd/spec/lib/cashregister_spec.rb
+++ objects_tdd/spec/lib/cashregister_spec.rb
@@ -1,28 +1,38 @@
 require './lib/cash_register'
+require 'rspec'
 
 describe CashRegister do
-  describe 'initialize' do
-    it 'returns 0.00 for a total price' do
-      expect(CashRegister.initialize).to eq(0.00)
+  describe '.total' do
+    it 'should return 0.00 for a starting total price' do
+      register = CashRegister.new
+      expect(register.total).to eq(0.00)
+    end
+  end
+end
+
+describe '.purchase' do
+  # context 'purchase' do
+  it 'should add amount to the total' do
+    register = CashRegister.new
+    register.purchase(3.78)
+    expect(register.total).to eq(3.78)
+  end
+
+  describe '.pay' do
+    # context 'purchase' do
+    it 'should subtract amount from the total' do
+      register = CashRegister.new
+      register.purchase(3.78)
+      register.pay(5)
+      # format('%.2f' % register.total)
+      expect(register.total).to eq(-1.22)
     end
 
-    describe '#purchase(price)' do
-      context 'purchase'
-      it 'returns sum of total and purchase price' do
-        def purchase(price)
-          @total += price
-        end
-        # expect(CashRegister.purchase(price)).to be_truthy # ??
-      end
-
-      describe '#pay(payment)' do
-        context 'pay'
-        it 'returns sum subtracted from total' do
-          def pay(payment)
-            @total -= payment
-          end
-          # expect(CashRegister.pay(payment)).to be_truthy # ??
-        end
+    describe '/.total' do
+      it 'should return reset total to 0.00' do
+        register = CashRegister.new
+        register.total
+        expect(register.total).to eq(0.00)
       end
     end
   end
